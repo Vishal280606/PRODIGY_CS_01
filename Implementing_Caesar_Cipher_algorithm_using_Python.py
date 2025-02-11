103c93b61d4c10d159298b4fef50b070ea5eff63
Vishal280606 <vishal280606@gmail.com>
2025-02-11 13:35:37 +0530

    Updated

--- Implementing_Caesar_Cipher_algorithm_using_Python.py
+++ Implementing_Caesar_Cipher_algorithm_using_Python.py
@@ -1,21 +1,21 @@
 def caesar_cipher_encrypt(text, shift): 
     encrypted_text = ""
     for char in text:
-        if char.isalpha():  # Check if the character is a letter
+        if char.isalpha():
             shift_base = 65 if char.isupper() else 97
             encrypted_text += chr((ord(char) - shift_base + shift) % 26 + shift_base)
         else:
-            encrypted_text += char  # Non-alphabet characters are not encrypted
+            encrypted_text += char
     return encrypted_text
 
 def caesar_cipher_decrypt(text, shift):
     decrypted_text = ""
     for char in text:
-        if char.isalpha():  # Check if the character is a letter
+        if char.isalpha():
             shift_base = 65 if char.isupper() else 97
             decrypted_text += chr((ord(char) - shift_base - shift) % 26 + shift_base)
         else:
-            decrypted_text += char  # Non-alphabet characters are not decrypted
+            decrypted_text += char
     return decrypted_text
 
 def main():
